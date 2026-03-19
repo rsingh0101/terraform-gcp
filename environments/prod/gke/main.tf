d492efa9df29cdadd88361cdcef0a1f11a50b1cc
rsingh0101 <singhratinder137@gmail.com>
2026-03-19 16:58:28 +0000

    Pipeline fixes

--- environments/prod/gke/main.tf
+++ environments/prod/gke/main.tf
@@ -8,95 +8,83 @@ provider "kubernetes" {
 }
 
 module "gke" {
-  source                     = "../../../modules/gke"
-  project_id                 = "<PROJECT ID>"
-  name                       = "gke-test-1"
-  region                     = "us-central1"
-  zones                      = ["us-central1-a", "us-central1-b", "us-central1-f"]
-  network                    = "vpc-01"
-  subnetwork                 = "us-central1-01"
-  ip_range_pods              = "us-central1-01-gke-01-pods"
-  ip_range_services          = "us-central1-01-gke-01-services"
-  http_load_balancing        = false
-  network_policy             = false
-  horizontal_pod_autoscaling = true
-  filestore_csi_driver       = false
-  enable_private_endpoint    = true
-  enable_private_nodes       = true
-  cloudrun                   = true
-  dns_cache                  = false
+  source         = "../../../modules/gke"
+  project_id     = "<PROJECT ID>"
+  name           = "gke-test-1"
+  location       = "us-central1"
+  node_locations = ["us-central1-a", "us-central1-b", "us-central1-f"]
+  network        = "vpc-01"
+  subnetwork     = "us-central1-01"
 
-  node_pools = [
+  # IP Allocation Policy for Pods and Services
+  ip_allocation_policy = {
+    cluster_secondary_range_name  = "us-central1-01-gke-01-pods"
+    services_secondary_range_name = "us-central1-01-gke-01-services"
+  }
+
+  master_authorized_networks_config = {
+    cidr_blocks                     = []
+    gcp_public_cidrs_access_enabled = false
+  }
+
+  workload_identity_config = {
+    workload_pool = "<PROJECT ID>.svc.id.goog"
+  }
+
+  addons_config = {
+    http_load_balancing             = { disabled = true }
+    horizontal_pod_autoscaling      = { disabled = false }
+    gcp_filestore_csi_driver_config = { enabled = false }
+    dns_cache_config                = { enabled = false }
+  }
+
+  node_pool = [
     {
-      name                       = "default-node-pool"
-      machine_type               = "e2-medium"
-      node_locations             = "us-central1-b,us-central1-c"
-      min_count                  = 1
-      max_count                  = 100
-      local_ssd_count            = 0
-      spot                       = false
-      local_ssd_ephemeral_count  = 0
-      disk_size_gb               = 100
-      disk_type                  = "pd-standard"
-      image_type                 = "COS_CONTAINERD"
-      enable_gcfs                = false
-      enable_gvnic               = false
-      logging_variant            = "DEFAULT"
-      auto_repair                = true
-      auto_upgrade               = true
-      service_account            = "project-service-account@<PROJECT ID>.iam.gserviceaccount.com"
-      preemptible                = false
-      initial_node_count         = 80
-      accelerator_count          = 1
-      accelerator_type           = "nvidia-l4"
-      gpu_driver_version         = "LATEST"
-      gpu_sharing_strategy       = "TIME_SHARING"
-      max_shared_clients_per_gpu = 2
-    },
+      name               = "default-node-pool"
+      initial_node_count = 80
+      node_locations     = ["us-central1-b", "us-central1-c"]
+
+      autoscaling = {
+        min_node_count = 1
+        max_node_count = 100
+      }
+
+      management = {
+        auto_repair  = true
+        auto_upgrade = true
+      }
+
+      node_config = {
+        machine_type    = "e2-medium"
+        image_type      = "COS_CONTAINERD"
+        disk_size_gb    = 100
+        disk_type       = "pd-standard"
+        service_account = "project-service-account@<PROJECT ID>.iam.gserviceaccount.com"
+        preemptible     = false
+        tags            = ["default-node-pool"]
+        labels          = { default-node-pool = "true" }
+        metadata        = { node-pool-metadata-custom-value = "my-node-pool" }
+        taint = [
+          {
+            key    = "default-node-pool"
+            value  = "true"
+            effect = "PREFER_NO_SCHEDULE"
+          }
+        ]
+        guest_accelerator = {
+          type  = "nvidia-l4"
+          count = 1
+          gpu_sharing_config = {
+            gpu_sharing_strategy       = "TIME_SHARING"
+            max_shared_clients_per_gpu = 2
+          }
+          gpu_driver_installation_config = {
+            gpu_driver_version = "LATEST"
+          }
+        }
+      }
+    }
   ]
-
-  node_pools_oauth_scopes = {
-    all = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
-
-  node_pools_labels = {
-    all = {}
-
-    default-node-pool = {
-      default-node-pool = true
-    }
-  }
-
-  node_pools_metadata = {
-    all = {}
-
-    default-node-pool = {
-      node-pool-metadata-custom-value = "my-node-pool"
-    }
-  }
-
-  node_pools_taints = {
-    all = []
-
-    default-node-pool = [
-      {
-        key    = "default-node-pool"
-        value  = true
-        effect = "PREFER_NO_SCHEDULE"
-      },
-    ]
-  }
-
-  node_pools_tags = {
-    all = []
-
-    default-node-pool = [
-      "default-node-pool",
-    ]
-  }
 }
 
 module "app_storage" {
